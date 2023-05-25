e96b75edf2b89ccc51910f1c152c0c278d1b3b05
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-05-26 00:26:35 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 const fs = require('fs/promises')
-
-const myFileWriter = async (fileName, fileContent) => {
-	// write code here
-	// dont chnage function name
+const fName = "myfile.txt"
+const fContent = "I wrote someting'"
+const myFileWriter = async(fileName,fileContent) =>{
+	await fs.writeFile(fileName,fileContent)
 }
+myFileWriter(fName,fContent)
 
-const myFileReader = async (fileName) => {
-	// write code here
-	// dont chnage function name
+
+const myFileReader = async (fileName) =>{
+	const content = await fs.readFile(fileName)
+	return content.toString();
 }
+myFileReader(fName).then(a=>console.log(a))
 
-
-const myFileUpdater = async (fileName, fileContent) => {
-	// write code here
-	// dont chnage function name
+const myFileUpdater = async (fileName,fileContent) => {
+	return await fs.appendFile(fileName,fileContent)
 }
+myFileUpdater(fName,"\nnew content")
 
 const myFileDeleter = async (fileName) => {
-	// write code here
-	// dont chnage function name
+	return await fs.unlink(fileName)
 }
+//myyFileDeleter(fName)
+
+
 
 
 
